7430a75b1981bc3bc105d4068426389086fa2e9f
moredark <sakmarkin01@gmail.com>
2024-09-28 19:31:50 +0300

    feat (frontend): Added PWA and icons

---
+++ frontend/app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Sila',
+    short_name: 'Sila',
+    description: '',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#000000',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      }
+    ],
+  }
+}
