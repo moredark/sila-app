226ec171d5ba8af81c50983c4761801f7b7a85c7
moredark <sakmarkin01@gmail.com>
2024-09-29 13:59:04 +0300

    fix (frontend): Fixed auth middleware (#8)

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -6,9 +6,12 @@ import { ACCESS_TOKEN_KEY } from '@/shared/config/auth'
 
 export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get(ACCESS_TOKEN_KEY)?.value
-
   if (request.nextUrl.pathname.startsWith('/auth')) {
-    return NextResponse.next()
+    if (accessToken) {
+      return NextResponse.redirect(new URL(APP_ROUTES.WORKOUT, request.url))
+    } else {
+      return NextResponse.next()
+    }
   }
 
   if (!accessToken) {
@@ -19,5 +22,14 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/profile', '/settings', '/workout'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
 }
