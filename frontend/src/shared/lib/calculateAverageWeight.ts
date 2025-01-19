c5d0146c64f1eb907b6dd179d3ba6f7889019b18
unkabas <undercannabis10@gmail.com>
2025-01-20 00:54:07 +0300

    fix(Average Weight) correct rounded weight

--- frontend/src/shared/lib/calculateAverageWeight.ts
+++ frontend/src/shared/lib/calculateAverageWeight.ts
@@ -4,7 +4,7 @@ export function calculateAverageWeight(sets: Set[]): number {
   if (sets.length === 0) return 0
 
   const totalWeight = sets.reduce((sum, set) => sum + set.weight!, 0)
-  const averageWeight = totalWeight / sets.length
+  const averageWeight = Math.round(totalWeight / sets.length)
 
   return averageWeight
 }
