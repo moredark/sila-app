09f395005a025cfd9613b754c15bdd356d45bcee
moredark <sakmarkin01@gmail.com>
2025-05-06 23:07:43 +0300

    feat(docs): init docs for all project

---
+++ docs/sidebars.js
@@ -0,0 +1,39 @@
+// @ts-check
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+
+ @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
+ */
+const sidebars = {
+  docs: [
+    'intro',
+    {
+      type: 'category',
+      label: 'Backend',
+      items: [
+        'backend/intro',
+        'backend/architecture',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Frontend',
+      items: [
+        'frontend/intro',
+        'frontend/fsd',
+      ],
+    },
+  ],
+};
+
+export default sidebars;
