d64f536cbfe205b2c053c1446c06b364ccb165c8
moredark <sakmarkin01@gmail.com>
2025-05-06 23:43:16 +0300

    fix(workout-pages): add ts-ignore in server components

--- frontend/app/(app)/workout/[id]/page.tsx
+++ frontend/app/(app)/workout/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { WorkoutSessionPage } from '@/views/workout-session/ui'
 
+// @ts-ignore: Ignore params typing issue with Next.js 15
 export default async function WorkoutSession({ params }: any) {
   const { id } = await params
 
